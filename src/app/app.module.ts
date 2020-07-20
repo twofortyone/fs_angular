18cd198604ee26cdd90a07206b696fc4396002de
paalpago <pa.parra12@uniandes.edu.co>
2020-07-20 16:39:22 -0500

    Angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,21 @@ import { MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule }  from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing/app-routing.module'; 
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +46,7 @@ import { FooterComponent } from './footer/footer.component';
     MatCardModule,
     MatButtonModule, 
     MatIconModule, 
-    FontAwesomeModule
+    FontAwesomeModule, 
   ],
   providers: [
     DishService,

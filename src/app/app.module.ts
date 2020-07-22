2ef85002f272d6825f6cdc86efeae91c43ab7802
paalpago <pa.parra12@uniandes.edu.co>
2020-07-21 22:40:12 -0500

    Reactive form part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 import 'hammerjs';
 
 // Components
@@ -63,7 +67,10 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     MatFormFieldModule, 
     MatInputModule,
-    MatCheckboxModule 
+    MatCheckboxModule, 
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPhone, faFax, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -8,13 +10,40 @@ import { faPhone, faFax, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 })
 export class ContactComponent implements OnInit {
 
+  // icons 
   telephone=  faPhone;
   fax = faFax;
   envelope= faEnvelope;
 
-  constructor() { }
+  // form variables 
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+
+  constructor(private fb: FormBuilder) { 
+
+    this.createForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  createForm(){
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnumber: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit(){
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
